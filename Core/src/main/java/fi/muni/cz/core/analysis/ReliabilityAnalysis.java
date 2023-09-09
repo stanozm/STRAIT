d506405e0cf705c346ca6635d070e85604a90463
Valtteri Valtonen <valtteri.valtonen@futurice.com>
2023-09-09 11:47:34 +0300

    Implement reliability analysis

--- Core/src/main/java/fi/muni/cz/core/analysis/ReliabilityAnalysis.java
+++ Core/src/main/java/fi/muni/cz/core/analysis/ReliabilityAnalysis.java
@@ -1,21 +1,43 @@
 package fi.muni.cz.core.analysis;
 
-import fi.muni.cz.core.configuration.ReliabilityAnalysisConfiguration;
+import fi.muni.cz.core.ArgsParser;
+import fi.muni.cz.core.analysis.phases.ReliabilityAnalysisPhase;
 import fi.muni.cz.core.dto.ReliabilityAnalysisDto;
+import java.util.List;
 
 /**
  * @author Valtteri Valtonen, valtonenvaltteri@gmail.com
  */
 public class ReliabilityAnalysis {
 
-    ReliabilityAnalysisConfiguration config;
+    private List<ReliabilityAnalysisPhase> phases;
+    private ArgsParser config;
 
-    public ReliabilityAnalysis(ReliabilityAnalysisConfiguration config){
-        this.config = config;
+    public ReliabilityAnalysis(ArgsParser config){
+        this.setConfig(config);
     }
 
     public ReliabilityAnalysisDto performAnalysis(ReliabilityAnalysisDto dto){
-        return null;
+        ReliabilityAnalysisDto currentDto = dto;
+        for(ReliabilityAnalysisPhase phase : phases){
+            currentDto = phase.execute(currentDto);
+        }
+        return currentDto;
     }
 
+    public List<ReliabilityAnalysisPhase> getPhases() {
+        return phases;
+    }
+
+    public void setPhases(List<ReliabilityAnalysisPhase> phases) {
+        this.phases = phases;
+    }
+
+    public ArgsParser getConfig() {
+        return config;
+    }
+
+    public void setConfig(ArgsParser config) {
+        this.config = config;
+    }
 }
