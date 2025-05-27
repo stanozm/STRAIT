1b902b3ea144ce438ee3987e0bdefcedb2eeb8fc
Andrej Mrázik <xmrazik3@fi.muni.cz>
2025-05-27 05:12:40 +0200

    Update: tests

--- Models/src/test/java/fi/muni/cz/models/leastsquaresolver/LeastSquaresSolverNGTest.java
+++ Models/src/test/java/fi/muni/cz/models/leastsquaresolver/LeastSquaresSolverNGTest.java
@@ -18,6 +18,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /** @author Radoslav Micko, 445611@muni.cz */
+/** @author Andrej Mrazik, 456651@muni.cz */
 public class LeastSquaresSolverNGTest {
 
   private final List<Pair<Integer, Integer>> listOfPairs = new ArrayList<>();
@@ -34,6 +35,10 @@ public class LeastSquaresSolverNGTest {
   private Solver yamadaExponentialSolver;
   private Solver yamadaRaleighSolver;
   private Solver logLogisticSolver;
+  private Solver phamNordmannZhangSolver;
+  private Solver phamZhangSolver;
+  private Solver wangSolver;
+  private Solver liSolver;
 
   @BeforeClass
   public void setUp() {
@@ -53,6 +58,10 @@ public class LeastSquaresSolverNGTest {
     yamadaExponentialSolver = new YamadaExponentialLeastSquaresSolver(rEngine, handler);
     yamadaRaleighSolver = new YamadaRaleighLeastSquaresSolver(rEngine, handler);
     logLogisticSolver = new LogLogisticLeastSquaresSolver(rEngine, handler);
+    phamNordmannZhangSolver = new PhamNordmannZhangLeastSquaresSolver(rEngine, handler);
+    phamZhangSolver = new PhamZhangLeastSquaresSolver(rEngine, handler);
+    wangSolver = new WangLeastSquaresSolver(rEngine, handler);
+    liSolver = new LiLeastSquaresSolver(rEngine, handler);
 
     // Set up mock behavior for RJriExceptionHandler
     // These mocks will handle the R operations that would be performed by the handler
@@ -60,6 +69,8 @@ public class LeastSquaresSolverNGTest {
     REXP twoParamRexp = new REXP(new double[] {1.0, 1.0});
     // For models with 3 parameters
     REXP threeParamRexp = new REXP(new double[] {1.0, 1.0, 1.0});
+    // For models with 4 parameters
+    REXP fourParamRexp = new REXP(new double[] {1.0, 1.0, 1.0, 1.0});
     // For model statistics
     REXP statRexp = new REXP(new int[] {1});
 
@@ -68,54 +79,67 @@ public class LeastSquaresSolverNGTest {
 
     // Special cases for specific model function calls
     when(handler.safeEval(any(Rengine.class), contains("coef(modelGO)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelGOS)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelDuane)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelMO)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelGO2)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelGOS2)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelDuane2)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelMO2)"), anyString()))
-        .thenReturn(twoParamRexp);
+            .thenReturn(twoParamRexp);
 
     when(handler.safeEval(any(Rengine.class), contains("coef(modelHD)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelWeibull)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelLogLogistic)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(
             any(Rengine.class), contains("coef(modelYamadaExponential)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelYamadaRaleigh)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelHD2)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelWeibull2)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelLogLogistic2)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(
             any(Rengine.class), contains("coef(modelYamadaExponential2)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
     when(handler.safeEval(any(Rengine.class), contains("coef(modelYamadaRaleigh2)"), anyString()))
-        .thenReturn(threeParamRexp);
+            .thenReturn(threeParamRexp);
+    when(handler.safeEval(any(Rengine.class), contains("coef(modelWang)"), anyString()))
+            .thenReturn(threeParamRexp);
+    when(handler.safeEval(any(Rengine.class), contains("coef(modelWang2)"), anyString()))
+            .thenReturn(threeParamRexp);
+
+    when(handler.safeEval(
+            any(Rengine.class), contains("coef(modelPhamNordmannZhang)"), anyString()))
+            .thenReturn(fourParamRexp);
+    when(handler.safeEval(any(Rengine.class), contains("coef(modelPhamZhang)"), anyString()))
+            .thenReturn(fourParamRexp);
+    when(handler.safeEval(any(Rengine.class), contains("coef(modelLi)"), anyString()))
+            .thenReturn(fourParamRexp);
+    when(handler.safeEval(
+            any(Rengine.class), contains("coef(modelPhamNordmannZhang2)"), anyString()))
+            .thenReturn(fourParamRexp);
+    when(handler.safeEval(any(Rengine.class), contains("coef(modelPhamZhang2)"), anyString()))
+            .thenReturn(fourParamRexp);
+    when(handler.safeEval(any(Rengine.class), contains("coef(modelLi2)"), anyString()))
+            .thenReturn(fourParamRexp);
 
     when(handler.safeEval(any(Rengine.class), contains("glance"), anyString()))
-        .thenReturn(statRexp);
+            .thenReturn(statRexp);
     when(handler.safeEval(any(Rengine.class), contains("R2nls"), anyString())).thenReturn(statRexp);
-
-    // Mock behavior for validateRexpResult - just do nothing
-    // This is a void method so we use Mockito.doNothing(), but it's the default behavior anyway
-
-    // Mock behavior for loadLibrary - just do nothing
-    // We don't need to stub this since it's a void method and we want it to do nothing
   }
 
   @Test
@@ -123,8 +147,10 @@ public class LeastSquaresSolverNGTest {
     // Set up expected results
     double[] doubleArr = {1.0, 1.0};
     double[] double3Arr = {1.0, 1.0, 1.0};
+    double[] double4Arr = {1.0, 1.0, 1.0, 1.0};
     int[] intArr = {1, 1};
     int[] int3Arr = {1, 1, 1};
+    int[] int4Arr = {1, 1, 1, 1};
 
     // Test solvers with 2 parameters
     assertEquals(GOSolver.optimize(intArr, listOfPairs).getParameters(), doubleArr);
@@ -137,7 +163,14 @@ public class LeastSquaresSolverNGTest {
     assertEquals(weibullSolver.optimize(int3Arr, listOfPairs).getParameters(), double3Arr);
     assertEquals(logLogisticSolver.optimize(int3Arr, listOfPairs).getParameters(), double3Arr);
     assertEquals(
-        yamadaExponentialSolver.optimize(int3Arr, listOfPairs).getParameters(), double3Arr);
+            yamadaExponentialSolver.optimize(int3Arr, listOfPairs).getParameters(), double3Arr);
     assertEquals(yamadaRaleighSolver.optimize(int3Arr, listOfPairs).getParameters(), double3Arr);
+    assertEquals(wangSolver.optimize(int3Arr, listOfPairs).getParameters(), double3Arr);
+
+    // Test solvers with 4 parameters
+    assertEquals(
+            phamNordmannZhangSolver.optimize(int4Arr, listOfPairs).getParameters(), double4Arr);
+    assertEquals(phamZhangSolver.optimize(int4Arr, listOfPairs).getParameters(), double4Arr);
+    assertEquals(liSolver.optimize(int4Arr, listOfPairs).getParameters(), double4Arr);
   }
 }
